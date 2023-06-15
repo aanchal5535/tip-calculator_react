71cd567cdcd1c2a98b57dea8f85996774eb6374b
aanchal-subedi <aanchalsubedi005@gmail.com>
2023-06-15 17:44:46 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tipcalculator from './component/tipcaculator/Tipcalculator'
+
+const App = () => {
+  return (
+    <div>
+      <Tipcalculator/>
+    </div>
+  )
+}
+
+export default App
